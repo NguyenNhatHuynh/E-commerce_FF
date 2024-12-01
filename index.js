434086abe8e37401c616e5396d73c7c34338f2de
Nhut Phi <phisuka0122386@gmail.com>
2024-12-01 19:19:16 +0700

    update2

--- index.js
+++ index.js
@@ -1,58 +1,75 @@
-const express = require("express")
+const express = require("express");
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
-const router = require("./router/client/mainrouter")
-const routeradmin = require("./router/admin/main")
-const routerauth = require("./router/auth/main")
-const dotevn = require("dotenv")
-const connect = require("./database/database")
-const system = require("./setting/system")
+const router = require("./router/client/mainrouter");
+const routeradmin = require("./router/admin/main");
+const routerauth = require("./router/auth/main");
+const dotevn = require("dotenv");
+const connect = require("./database/database");
+const system = require("./setting/system");
 const http = require('http');
 const { Server } = require("socket.io");
 const flash = require('express-flash');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const path = require('path');
+const passport = require('passport');
+const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
+
 dotevn.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Sử dụng port từ .env nếu có
 const server = http.createServer(app);
 const io = new Server(server);
-global._io = io
-const passport = require('passport');
+global._io = io;
+
+const redisClient = redis.createClient({
+  host: 'localhost',
+  port: 6379,
+});
+
+// Cấu hình session với Redis
 app.use(session({
-  secret: 'keyboard cat',
+  store: new RedisStore({ client: redisClient }),
+  secret: 'your-secret',
+  cookie: { secure: false, maxAge: 3600000 },
   resave: false,
-  saveUninitialized: false,
+  saveUninitialized: false
 }));
-// Cấu hình express-session
-app.use(passport.authenticate('session'));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
+
 app.set("views", `${__dirname}/views`);
-app.set("view engine", "pug")
+app.set("view engine", "pug");
+
 app.use(cookieParser('KJJSLKASASASA'));
-app.use(session({
-    secret: 'ádasd', // Chọn một giá trị bảo mật ngẫu nhiên
-    cookie: { maxAge: 60000 },
-    resave: false,
-    saveUninitialized: true // Đặt thành true hoặc false tùy thuộc vào yêu cầu của bạn
-  }));
 app.use(flash());
-app.locals.prefixAdmin = system.prefixAdmin
+app.locals.prefixAdmin = system.prefixAdmin;
+
 app.use(express.static(`${__dirname}/public`));
 app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+
+// Định nghĩa các routes
 router(app);
 routeradmin(app);
 routerauth(app);
+
+// Kết nối cơ sở dữ liệu
 connect.connect();
+
+// Xử lý lỗi 404
 app.get("*", (req, res) => {
   res.render("client/page/errors/index", {
     pageTitle: "404 Not Found",
   });
 });
+
 server.listen(port, () => {
-    console.log('Đây là app của tôi')
-})
+    console.log(`App đang chạy tại http://localhost:${port}`);
+});
